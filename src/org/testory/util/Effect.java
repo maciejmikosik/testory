fdcb423849031eba60368a91560cce41911b78c8
Maciej Mikosik <maciej@mikosik.com>
2014-01-23 19:43:33 +0100

    created Effect

---
+++ src/org/testory/util/Effect.java
@@ -0,0 +1,62 @@
+package org.testory.util;
+
+import static org.testory.common.Checks.checkArgument;
+import static org.testory.common.Checks.checkNotNull;
+
+import org.testory.common.Nullable;
+
+public class Effect {
+  private static enum Mode {
+    returnedObject, returnedVoid, thrown
+  }
+
+  private final Mode mode;
+  private final Object returned;
+  private final Throwable thrown;
+
+  private Effect(Mode mode, Object returned, Throwable thrown) {
+    this.mode = mode;
+    this.returned = returned;
+    this.thrown = thrown;
+  }
+
+  public static Effect returned(@Nullable Object object) {
+    return new Effect(Mode.returnedObject, object, null);
+  }
+
+  public static Effect returnedVoid() {
+    return new Effect(Mode.returnedVoid, null, null);
+  }
+
+  public static Effect thrown(Throwable throwable) {
+    checkNotNull(throwable);
+    return new Effect(Mode.thrown, null, throwable);
+  }
+
+  public static boolean hasReturned(Effect effect) {
+    return effect.mode == Mode.returnedObject || effect.mode == Mode.returnedVoid;
+  }
+
+  public static boolean hasReturnedObject(Effect effect) {
+    return effect.mode == Mode.returnedObject;
+  }
+
+  public static boolean hasReturnedVoid(Effect effect) {
+    return effect.mode == Mode.returnedVoid;
+  }
+
+  public static boolean hasThrown(Effect effect) {
+    return effect.mode == Mode.thrown;
+  }
+
+  @Nullable
+  public static Object getReturned(Effect effect) {
+    checkArgument(effect.mode == Mode.returnedObject);
+    return effect.returned;
+  }
+
+  public static Throwable getThrown(Effect effect) {
+    checkArgument(effect.mode == Mode.thrown);
+    return effect.thrown;
+  }
+}
