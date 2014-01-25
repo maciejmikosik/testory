56c4faff1f5bbee71e1041d6cab961f13cc76806
Maciej Mikosik <maciej@mikosik.com>
2014-01-25 13:59:53 +0100

    refactored History as purgeable event list

--- src/org/testory/History.java
+++ src/org/testory/History.java
@@ -2,20 +2,52 @@ package org.testory;
 
 import static org.testory.common.Checks.checkNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testory.util.Effect;
 
 public class History {
-  private final ThreadLocal<Effect> data = new ThreadLocal<Effect>();
+  /** youngest at begin */
+  private final ThreadLocal<List<Object>> threadLocalEvents = new ThreadLocal<List<Object>>() {
+    protected List<Object> initialValue() {
+      return new ArrayList<Object>();
+    }
+  };
 
   public History() {}
 
+  private List<Object> getEvents() {
+    return new ArrayList<Object>(threadLocalEvents.get());
+  }
+
+  private void setEvents(List<Object> events) {
+    threadLocalEvents.set(new ArrayList<Object>(events));
+  }
+
+  private void addEvent(Object event) {
+    threadLocalEvents.get().add(0, event);
+  }
+
   public void logWhen(Effect effect) {
     checkNotNull(effect);
-    data.set(effect);
+
+    List<Object> events = getEvents();
+    for (int i = 0; i < events.size(); i++) {
+      if (events.get(i) instanceof Effect) {
+        setEvents(events.subList(0, i));
+      }
+    }
+    addEvent(effect);
   }
 
   public Effect getLastWhenEffect() {
-    Effect effect = data.get();
+    Effect effect = null;
+    for (Object event : getEvents()) {
+      if (event instanceof Effect) {
+        effect = (Effect) event;
+      }
+    }
     check(effect != null);
     return effect;
   }
