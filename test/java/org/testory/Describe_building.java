e374d47ac00bd64afd2c05ca717648ca12d31776
Maciej Mikosik <maciej@mikosik.com>
2014-10-16 23:56:59 +0200

    added testory source files to eclipse classpath

---
+++ test/java/org/testory/Describe_building.java
@@ -0,0 +1,18 @@
+package org.testory;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class Describe_building {
+  @Test
+  public void source_code_is_available_at_runtime() throws IOException {
+    String sourceFile = Testory.class.getSimpleName() + ".java";
+    InputStream stream = Testory.class.getResourceAsStream(sourceFile);
+    assertNotNull(stream);
+    stream.close();
+  }
+}
