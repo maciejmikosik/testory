6ef04611b2c3b0368b33af7da7c66d9f845d5ea7
Maciej Mikosik <maciej@mikosik.com>
2014-10-21 21:58:22 +0200

    decorated TestoryException message with failed precondition

--- main/java/org/testory/TestoryException.java
+++ main/java/org/testory/TestoryException.java
@@ -1,5 +1,11 @@
 package org.testory;
 
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.testory.proxy.Proxies;
 
 public class TestoryException extends RuntimeException {
@@ -20,7 +26,7 @@ public class TestoryException extends RuntimeException {
   }
 
   public static void check(boolean condition) {
-    check(condition, "incorrect usage in client code");
+    check(condition, "illegal testory usage");
   }
 
   public static void check(boolean condition, String message) {
@@ -29,21 +35,30 @@ public class TestoryException extends RuntimeException {
     }
   }
 
-  private static TestoryException decorate(String callerMessage, TestoryException original) {
-    TestoryException check = new TestoryException("failed precondition in testory code", original);
-    check.setStackTrace(failedCheckTrace(original.getStackTrace()));
-    TestoryException caller = new TestoryException(callerMessage, check);
-    caller.setStackTrace(callerTrace(original.getStackTrace()));
-    return caller;
+  private static TestoryException decorate(String message, TestoryException exception) {
+    StackTraceElement[] trace = exception.getStackTrace();
+    StackTraceElement[] digestTrace = array(findPrecondition(trace), findCaller(trace));
+    String decoratedMessage = "" //
+        + "\n" //
+        + "  " + message + "\n" //
+        + "  failed precondition\n" //
+        + "    " + trimCondition(sourceLine(findPrecondition(trace))) + "\n";
+    TestoryException decorated = new TestoryException(decoratedMessage, exception);
+    decorated.setStackTrace(digestTrace);
+    return decorated;
   }
 
-  private static StackTraceElement[] callerTrace(StackTraceElement[] stackTrace) {
+  private static String trimCondition(String line) {
+    return line.trim().replace("check(", "").replace(");", "");
+  }
+
+  private static StackTraceElement findCaller(StackTraceElement[] stackTrace) {
     for (int i = stackTrace.length - 1; i >= 0; i--) {
       String name = stackTrace[i].getClassName();
       if (name.equals(Testory.class.getName()) || name.startsWith(Proxies.class.getName())) {
         for (int j = i + 1; j < stackTrace.length; j++) {
           if (stackTrace[j].getLineNumber() >= 0) {
-            return new StackTraceElement[] { stackTrace[j] };
+            return stackTrace[j];
           }
         }
         throw new Error();
@@ -52,13 +67,49 @@ public class TestoryException extends RuntimeException {
     throw new Error();
   }
 
-  private static StackTraceElement[] failedCheckTrace(StackTraceElement[] stackTrace) {
+  private static StackTraceElement findPrecondition(StackTraceElement[] stackTrace) {
     for (int i = stackTrace.length - 1; i >= 0; i--) {
       if (stackTrace[i].getClassName().equals(TestoryException.class.getName())
           && stackTrace[i].getMethodName().equals("check")) {
-        return new StackTraceElement[] { stackTrace[i + 1] };
+        return stackTrace[i + 1];
       }
     }
     throw new Error();
+  }
+
+  private static String sourceLine(StackTraceElement trace) {
+    int lineNumber = trace.getLineNumber();
+    BufferedReader reader = null;
+    try {
+      Class<?> type = Class.forName(trace.getClassName());
+      while (type.getEnclosingClass() != null) {
+        type = type.getEnclosingClass();
+      }
+      String simpleName = type.getSimpleName();
+      InputStream resourceAsStream = type.getResourceAsStream(simpleName + ".java");
+      reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+      for (int i = 1; i < lineNumber; i++) {
+        reader.readLine();
+      }
+      return reader.readLine().trim();
+    } catch (ClassNotFoundException e) {
+      throw new Error(e);
+    } catch (IOException e) {
+      throw new Error(e);
+    } finally {
+      closeQuietly(reader);
+    }
+  }
+
+  private static void closeQuietly(Closeable closeable) {
+    try {
+      if (closeable != null) {
+        closeable.close();
+      }
+    } catch (IOException e) {}
+  }
+
+  private static <T> T[] array(T... values) {
+    return values;
   }
 }
--- test/java/org/testory/Describe_TestoryException.java
+++ test/java/org/testory/Describe_TestoryException.java
@@ -12,31 +12,55 @@ import org.junit.Test;
 
 public class Describe_TestoryException {
   private StackTraceElement line;
+  private StackTraceElement trace;
 
   @Test
-  public void reports_caller_code() {
+  public void message_conforms_to_template() {
+    try {
+      mock(null);
+      fail();
+    } catch (TestoryException e) {
+      assertTrue(e.getMessage(), e.getMessage().matches("" //
+          + "\n" //
+          + "  illegal testory usage\n" //
+          + "  failed precondition\n" //
+          + "    .*\n"));
+    }
+  }
+
+  @Test
+  public void message_contains_failed_precondition() {
+    try {
+      mock(null);
+      fail();
+    } catch (TestoryException e) {
+      assertTrue(e.getMessage(), !e.getMessage().contains("check"));
+      assertTrue(e.getMessage(), e.getMessage().contains("type != null"));
+    }
+  }
+
+  @Test
+  public void reports_caller_trace() {
     try {
       line = nextLine(here());
       mock(null);
       fail();
     } catch (TestoryException e) {
-      assertEquals("incorrect usage in client code", e.getMessage());
-      assertEquals(1, e.getStackTrace().length);
-      assertEquals(line, e.getStackTrace()[0]);
+      trace = e.getStackTrace()[1];
+      assertEquals(this.getClass().getName(), trace.getClassName());
+      assertEquals(line, trace);
     }
   }
 
   @Test
-  public void reports_failed_check() {
+  public void reports_failed_precondition_trace() {
     try {
       mock(null);
       fail();
     } catch (TestoryException e) {
-      Throwable cause = e.getCause();
-      assertEquals("failed precondition in testory code", cause.getMessage());
-      assertEquals(1, cause.getStackTrace().length);
-      assertEquals(Testory.class.getName(), cause.getStackTrace()[0].getClassName());
-      assertEquals("mock", cause.getStackTrace()[0].getMethodName());
+      trace = e.getStackTrace()[0];
+      assertEquals(Testory.class.getName(), trace.getClassName());
+      assertEquals("mock", trace.getMethodName());
     }
   }
 
@@ -46,9 +70,9 @@ public class Describe_TestoryException {
       mock(null);
       fail();
     } catch (TestoryException e) {
-      Throwable cause = e.getCause().getCause();
+      Throwable cause = e.getCause();
       assertNull(cause.getMessage());
-      assertTrue(cause.getStackTrace().length > 1);
+      assertTrue(cause.getStackTrace().length > 2);
       assertNull(cause.getCause());
     }
   }
