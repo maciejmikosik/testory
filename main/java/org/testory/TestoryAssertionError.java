effab3274b3929ba613f23bc3a11041985cd601a
Maciej Mikosik <maciej@mikosik.com>
2017-04-26 13:24:06 +0200

    disabled rewriting of stack trace in assertion error

--- main/java/org/testory/TestoryAssertionError.java
+++ main/java/org/testory/TestoryAssertionError.java
@@ -1,7 +1,5 @@
 package org.testory;
 
-import org.testory.proxy.proxer.CglibProxer;
-
 public class TestoryAssertionError extends AssertionError {
   public TestoryAssertionError() {}
 
@@ -10,26 +8,6 @@ public class TestoryAssertionError extends AssertionError {
   }
 
   public static TestoryAssertionError assertionError(String message) {
-    TestoryAssertionError original = new TestoryAssertionError();
-    TestoryAssertionError caller = new TestoryAssertionError(message);
-    caller.initCause(original);
-    caller.setStackTrace(callerTrace(original.getStackTrace()));
-    return caller;
-  }
-
-  private static StackTraceElement[] callerTrace(StackTraceElement[] stackTrace) {
-    for (int i = stackTrace.length - 1; i >= 0; i--) {
-      // TODO implement more generic way that handles other Proxers
-      String name = stackTrace[i].getClassName();
-      if (name.equals(Testory.class.getName()) || name.startsWith(CglibProxer.class.getName())) {
-        for (int j = i + 1; j < stackTrace.length; j++) {
-          if (stackTrace[j].getLineNumber() >= 0) {
-            return new StackTraceElement[] { stackTrace[j] };
-          }
-        }
-        throw new Error();
-      }
-    }
-    throw new Error();
+    return new TestoryAssertionError(message);
   }
 }
