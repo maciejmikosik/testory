b2e155a755d5320e26c211e6c8346697d392e76d
Maciej Mikosik <maciej@mikosik.com>
2017-04-26 21:02:29 +0200

    created common formatter

---
+++ main/java/org/testory/common/Formatter.java
@@ -0,0 +1,65 @@
+package org.testory.common;
+
+import static java.lang.String.join;
+
+import java.lang.reflect.Array;
+import java.util.Iterator;
+
+public class Formatter {
+  protected Formatter() {}
+
+  public static Formatter formatter() {
+    return new Formatter();
+  }
+
+  public String format(@Nullable Object object) {
+    if (object == null) {
+      return "null";
+    } else if (object.getClass().isArray()) {
+      return formatArray(object);
+    } else {
+      return String.valueOf(object);
+    }
+  }
+
+  protected Iterable<String> formatSequence(final Iterable<?> iterable) {
+    return new Iterable<String>() {
+      public Iterator<String> iterator() {
+        return new Iterator<String>() {
+          Iterator<?> iterator = iterable.iterator();
+
+          public boolean hasNext() {
+            return iterator.hasNext();
+          }
+
+          public String next() {
+            return format(iterator.next());
+          }
+        };
+      }
+    };
+  }
+
+  private String formatArray(Object array) {
+    return "[" + join(", ", formatSequence(arrayAsIterable(array))) + "]";
+  }
+
+  private Iterable<Object> arrayAsIterable(final Object array) {
+    return new Iterable<Object>() {
+      public Iterator<Object> iterator() {
+        return new Iterator<Object>() {
+          final int size = Array.getLength(array);
+          int index = 0;
+
+          public boolean hasNext() {
+            return index < size;
+          }
+
+          public Object next() {
+            return Array.get(array, index++);
+          }
+        };
+      }
+    };
+  }
+}
