9598a4d7ce711c3fa439484460555e3070b6b2b8
Maciej Mikosik <maciej@mikosik.com>
2018-01-06 20:19:26 +0100

    fixed purging corner cases

--- main/java/org/testory/facade/PurgingFacade.java
+++ main/java/org/testory/facade/PurgingFacade.java
@@ -5,6 +5,8 @@ import static org.testory.plumbing.PlumbingException.check;
 import static org.testory.proxy.Invocation.invocation;
 import static org.testory.proxy.Typing.implementing;
 
+import java.lang.reflect.Method;
+
 import org.testory.common.Chain;
 import org.testory.plumbing.history.History;
 import org.testory.proxy.Handler;
@@ -12,50 +14,57 @@ import org.testory.proxy.Invocation;
 import org.testory.proxy.Proxer;
 
 public class PurgingFacade {
-  private enum Word {
-    GIVEN, WHEN, THEN
-  }
-
   public static Facade purging(final History history, Proxer proxer, final Facade facade) {
     check(history != null);
     check(proxer != null);
     check(facade != null);
     return (Facade) proxer.proxy(implementing(Facade.class), new Handler() {
+      private Word lastWord = Word.GIVEN;
+      private Chain<Object> lastEvents = chain();
+
       public Object handle(Invocation invocation) throws Throwable {
-        String methodName = invocation.method.getName();
-        if (methodName.startsWith("given")) {
-          given();
-        } else if (methodName.startsWith("when")) {
-          when();
-        } else if (methodName.startsWith("then")) {
-          then();
+        Word word = wordFor(invocation.method);
+        if (word != null) {
+          if (requiresPurge(lastWord, word)) {
+            purge();
+          }
+          lastWord = word;
+          lastEvents = history.get();
         }
         return invocation(invocation.method, facade, invocation.arguments).invoke();
       }
 
-      private Word lastWord = Word.GIVEN;
-      private Chain<Object> events = chain();
-
-      private void given() {
-        if (lastWord != Word.GIVEN) {
-          history.cut(events);
+      private void purge() {
+        Chain<Object> chain = history.get();
+        while (chain.size() > 0) {
+          if (chain == lastEvents || chain.get() instanceof Invocation) {
+            history.cut(chain);
+            break;
+          }
+          chain = chain.remove();
         }
-        lastWord = Word.GIVEN;
-        events = history.get();
-      }
-
-      private void when() {
-        if (lastWord != Word.GIVEN) {
-          history.cut(events);
-        }
-        lastWord = Word.WHEN;
-        events = history.get();
-      }
-
-      private void then() {
-        lastWord = Word.THEN;
-        events = history.get();
       }
     });
+  }
+
+  private static Word wordFor(Method method) {
+    String methodName = method.getName();
+    if (methodName.startsWith("given")) {
+      return Word.GIVEN;
+    } else if (methodName.startsWith("when")) {
+      return Word.WHEN;
+    } else if (methodName.startsWith("then")) {
+      return Word.THEN;
+    } else {
+      return null;
+    }
+  }
+
+  private static boolean requiresPurge(Word previous, Word next) {
+    return previous != Word.GIVEN && next != Word.THEN;
+  }
+
+  private enum Word {
+    GIVEN, WHEN, THEN
   }
 }
