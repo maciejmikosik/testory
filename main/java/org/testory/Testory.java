3aea990c6101093bf8d2c981d7c1e7cd258994d0
Maciej Mikosik <maciej@mikosik.com>
2018-01-18 21:16:44 +0100

    extracted wiring from testory to testory facade

--- main/java/org/testory/Testory.java
+++ main/java/org/testory/Testory.java
@@ -1,83 +1,18 @@
 package org.testory;
 
-import static org.testory.plumbing.Checker.checker;
-import static org.testory.plumbing.CheckingProxer.checkingProxer;
-import static org.testory.plumbing.facade.ConfigurableFacade.configurableFacade;
-import static org.testory.plumbing.facade.Configuration.configuration;
-import static org.testory.plumbing.facade.PurgingFacade.purging;
-import static org.testory.plumbing.format.MessageFormatter.messageFormatter;
-import static org.testory.plumbing.format.QuietFormatter.quiet;
-import static org.testory.plumbing.history.RawHistory.newRawHistory;
-import static org.testory.plumbing.history.SynchronizedHistory.synchronize;
-import static org.testory.plumbing.im.wildcard.Repairer.repairer;
-import static org.testory.plumbing.im.wildcard.Tokenizer.tokenizer;
-import static org.testory.plumbing.im.wildcard.WildcardMatcherizer.wildcardMatcherizer;
-import static org.testory.plumbing.im.wildcard.WildcardSupport.wildcardSupport;
-import static org.testory.plumbing.inject.ArrayMaker.singletonArray;
-import static org.testory.plumbing.inject.ChainedMaker.chain;
-import static org.testory.plumbing.inject.FinalMaker.finalMaker;
-import static org.testory.plumbing.inject.Injector.injector;
-import static org.testory.plumbing.inject.RandomPrimitiveMaker.randomPrimitiveMaker;
-import static org.testory.plumbing.mock.NiceMockMaker.nice;
-import static org.testory.plumbing.mock.RawMockMaker.rawMockMaker;
-import static org.testory.plumbing.mock.SaneMockMaker.sane;
-import static org.testory.plumbing.mock.UniqueNamer.uniqueNamer;
-import static org.testory.proxy.proxer.CglibProxer.cglibProxer;
-import static org.testory.proxy.proxer.FixObjectBugProxer.fixObjectBug;
-import static org.testory.proxy.proxer.JdkCollectionsProxer.jdkCollections;
-import static org.testory.proxy.proxer.NonFinalProxer.nonFinal;
-import static org.testory.proxy.proxer.RepeatableProxy.repeatable;
-import static org.testory.proxy.proxer.TypeSafeProxer.typeSafe;
-import static org.testory.proxy.proxer.WrappingProxer.wrapping;
+import static org.testory.TestoryFacade.testoryFacade;
 
 import org.testory.common.Closure;
 import org.testory.common.Nullable;
 import org.testory.common.VoidClosure;
-import org.testory.plumbing.Checker;
-import org.testory.plumbing.Maker;
-import org.testory.plumbing.facade.Configuration;
 import org.testory.plumbing.facade.Facade;
-import org.testory.plumbing.format.QuietFormatter;
-import org.testory.plumbing.history.History;
-import org.testory.plumbing.im.wildcard.WildcardSupport;
-import org.testory.plumbing.inject.Injector;
-import org.testory.plumbing.mock.Namer;
 import org.testory.proxy.Handler;
 import org.testory.proxy.InvocationMatcher;
-import org.testory.proxy.Proxer;
 
 public class Testory {
   private static final ThreadLocal<Facade> localFacade = new ThreadLocal<Facade>() {
     protected Facade initialValue() {
-      Class<TestoryException> exception = TestoryException.class;
-      History loudHistory = synchronize(newRawHistory());
-      QuietFormatter formatter = quiet(messageFormatter());
-      History history = formatter.quiet(loudHistory);
-      Checker checker = checker(history, exception);
-      Proxer proxer = wrapping(exception,
-          nonFinal(typeSafe(jdkCollections(fixObjectBug(repeatable(
-              wrapping(exception, cglibProxer())))))));
-      Namer mockNamer = uniqueNamer(history);
-      Maker mockMaker = sane(history, nice(history, rawMockMaker(history, checkingProxer(checker, proxer))));
-      Injector injector = injector(singletonArray(chain(randomPrimitiveMaker(), finalMaker(), mockMaker)));
-      WildcardSupport wildcardSupport = wildcardSupport(
-          history,
-          tokenizer(proxer),
-          repairer(),
-          wildcardMatcherizer(formatter),
-          formatter);
-      Configuration configuration = configuration()
-          .history(history)
-          .formatter(formatter)
-          .exception(exception)
-          .checker(checker)
-          .proxer(proxer)
-          .mockNamer(mockNamer)
-          .mockMaker(mockMaker)
-          .injector(injector)
-          .wildcardSupport(wildcardSupport)
-          .validate();
-      return purging(history, cglibProxer(), configurableFacade(configuration));
+      return testoryFacade();
     }
   };
 
