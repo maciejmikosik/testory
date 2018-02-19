600e8ac093862f34ca7f503928ec4cfbf8658831
Maciej Mikosik <maciej@mikosik.com>
2018-02-19 18:05:03 -0500

    used bytecode generation to implement verifier

--- main/java/org/testory/plumbing/VerifyingInOrder.java
+++ main/java/org/testory/plumbing/VerifyingInOrder.java
@@ -1,12 +1,8 @@
 package org.testory.plumbing;
 
-import static org.testory.common.Chain.chain;
 import static org.testory.plumbing.PlumbingException.check;
 
 import org.testory.common.Chain;
-import org.testory.common.Optional;
-import org.testory.proxy.Invocation;
-import org.testory.proxy.InvocationMatcher;
 
 public class VerifyingInOrder {
   public final Chain<Object> unverified;
@@ -18,30 +14,5 @@ public class VerifyingInOrder {
   public static VerifyingInOrder verifyingInOrder(Chain<Object> unverified) {
     check(unverified != null);
     return new VerifyingInOrder(unverified);
-  }
-
-  public static Optional<VerifyingInOrder> verifyInOrder(InvocationMatcher invocationMatcher, Chain<Object> history) {
-    Chain<Object> unverified = unverifiedReversed(history);
-    while (unverified.size() > 0) {
-      Object event = unverified.get();
-      unverified = unverified.remove();
-      if (event instanceof Invocation && invocationMatcher.matches((Invocation) event)) {
-        return Optional.of(verifyingInOrder(unverified.reverse()));
-      }
-    }
-    return Optional.empty();
-  }
-
-  private static Chain<Object> unverifiedReversed(Chain<Object> history) {
-    Chain<Object> unverified = chain();
-    for (Object event : history) {
-      if (event instanceof VerifyingInOrder) {
-        unverified = unverified.addAll(((VerifyingInOrder) event).unverified);
-        break;
-      } else {
-        unverified = unverified.add(event);
-      }
-    }
-    return unverified;
   }
 }
--- main/java/org/testory/plumbing/verify/Verifier.java
+++ main/java/org/testory/plumbing/verify/Verifier.java
@@ -1,21 +1,20 @@
 package org.testory.plumbing.verify;
 
 import static org.testory.TestoryAssertionError.assertionError;
+import static org.testory.common.Chain.chain;
 import static org.testory.common.Classes.defaultValue;
+import static org.testory.common.Collections.last;
 import static org.testory.common.Matchers.asMatcher;
 import static org.testory.plumbing.PlumbingException.check;
-import static org.testory.plumbing.VerifyingInOrder.verifyInOrder;
+import static org.testory.plumbing.VerifyingInOrder.verifyingInOrder;
 import static org.testory.plumbing.format.Body.body;
 import static org.testory.plumbing.format.Header.header;
 import static org.testory.plumbing.format.Multiline.multiline;
 import static org.testory.plumbing.history.FilteredHistory.filter;
-import static org.testory.proxy.Invocation.invocation;
 import static org.testory.proxy.Typing.implementing;
 
-import java.lang.reflect.Method;
-
+import org.testory.common.Chain;
 import org.testory.common.Matcher;
-import org.testory.common.Optional;
 import org.testory.common.PageFormatter;
 import org.testory.plumbing.VerifyingInOrder;
 import org.testory.plumbing.facade.Facade;
@@ -29,135 +28,108 @@ import org.testory.proxy.Proxer;
 import org.testory.proxy.extra.Overrider;
 
 public class Verifier {
-  private final Overrider overrider;
-  private final PageFormatter pageFormatter;
-  private final WildcardSupport wildcardSupport;
-  private final History history;
-  private final FilteredHistory<Invocation> invocationHistory;
-
-  private Verifier(
-      Overrider overrider,
-      PageFormatter pageFormatter,
-      WildcardSupport wildcardSupport,
-      History history,
-      FilteredHistory<Invocation> invocationHistory) {
-    this.overrider = overrider;
-    this.pageFormatter = pageFormatter;
-    this.wildcardSupport = wildcardSupport;
-    this.history = history;
-    this.invocationHistory = invocationHistory;
-  }
-
   public static Facade verifier(
       Proxer proxer,
-      Overrider overrider,
-      PageFormatter pageFormatter,
-      WildcardSupport wildcardSupport,
-      History history) {
+      final Overrider overrider,
+      final PageFormatter pageFormatter,
+      final WildcardSupport wildcardSupport,
+      final History history) {
     check(proxer != null);
     check(overrider != null);
     check(pageFormatter != null);
     check(wildcardSupport != null);
     check(history != null);
-    final Verifier verifier = new Verifier(
-        overrider,
-        pageFormatter,
-        wildcardSupport,
-        history,
-        filter(Invocation.class, history));
+    final FilteredHistory<Invocation> invocationHistory = filter(Invocation.class, history);
     return (Facade) proxer.proxy(implementing(Facade.class), new Handler() {
-      public Object handle(Invocation invocation) throws Throwable {
-        check(invocation.method.getName().startsWith("thenCalled"));
-        Method method = Verifier.class.getMethod(
-            invocation.method.getName(),
-            invocation.method.getParameterTypes());
-        return invocation(method, verifier, invocation.arguments).invoke();
+      public Object handle(final Invocation thenCalledInvocation) throws Throwable {
+        check(thenCalledInvocation.method.getName().startsWith("thenCalled"));
+        if (last(thenCalledInvocation.method.getParameterTypes()) == InvocationMatcher.class) {
+          verify(thenCalledInvocation, (InvocationMatcher) last(thenCalledInvocation.arguments));
+          return null;
+        } else {
+          return overrider.override(last(thenCalledInvocation.arguments), new Handler() {
+            public Object handle(Invocation invocation) {
+              verify(thenCalledInvocation, wildcardSupport.matcherize(invocation));
+              return defaultValue(invocation.method.getReturnType());
+            }
+          });
+        }
+      }
+
+      private void verify(Invocation thenCalledInvocation, InvocationMatcher invocationMatcher) {
+        if (thenCalledInvocation.method.getName().endsWith("InOrder")) {
+          thenCalledInOrder(invocationMatcher);
+        } else {
+          thenCalledTimes(numberMatcher(thenCalledInvocation), invocationMatcher);
+        }
+      }
+
+      private void thenCalledTimes(Matcher numberMatcher, InvocationMatcher invocationMatcher) {
+        int numberOfCalls = 0;
+        for (Invocation invocation : invocationHistory.get()) {
+          if (invocationMatcher.matches(invocation)) {
+            numberOfCalls++;
+          }
+        }
+        if (!numberMatcher.matches(numberOfCalls)) {
+          throw assertionError(pageFormatter
+              .add(header("expected called times " + numberMatcher))
+              .add(body(invocationMatcher))
+              .add(header("but called"))
+              .add(body("times " + numberOfCalls))
+              .add(header("actual invocations"))
+              .add(invocationHistory.get().size() > 0
+                  ? multiline(invocationHistory.get().reverse())
+                  : body("none"))
+              .build());
+        }
+      }
+
+      private void thenCalledInOrder(InvocationMatcher invocationMatcher) {
+        Chain<Object> unverifiedOldest = chain();
+        for (Object event : history.get()) {
+          if (event instanceof VerifyingInOrder) {
+            unverifiedOldest = unverifiedOldest.addAll(((VerifyingInOrder) event).unverified);
+            break;
+          } else {
+            unverifiedOldest = unverifiedOldest.add(event);
+          }
+        }
+        while (unverifiedOldest.size() > 0) {
+          Object event = unverifiedOldest.get();
+          unverifiedOldest = unverifiedOldest.remove();
+          if (event instanceof Invocation && invocationMatcher.matches((Invocation) event)) {
+            history.add(verifyingInOrder(unverifiedOldest.reverse()));
+            return;
+          }
+        }
+        throw assertionError(pageFormatter
+            .add(header("expected called in order"))
+            .add(body(invocationMatcher))
+            .add(header("but not called"))
+            .add(header("actual invocations"))
+            .add(invocationHistory.get().size() > 0
+                ? multiline(invocationHistory.get().reverse())
+                : body("none"))
+            .build());
       }
     });
   }
 
-  public <T> T thenCalled(T mock) {
-    return thenCalledTimes(exactly(1), mock);
-  }
-
-  public void thenCalled(InvocationMatcher invocationMatcher) {
-    thenCalledTimes(exactly(1), invocationMatcher);
-  }
-
-  public <T> T thenCalledNever(T mock) {
-    return thenCalledTimes(exactly(0), mock);
-  }
-
-  public void thenCalledNever(InvocationMatcher invocationMatcher) {
-    thenCalledTimes(exactly(0), invocationMatcher);
-  }
-
-  public <T> T thenCalledTimes(int number, T mock) {
-    return thenCalledTimes(exactly(number), mock);
-  }
-
-  public void thenCalledTimes(int number, InvocationMatcher invocationMatcher) {
-    thenCalledTimes(exactly(number), invocationMatcher);
-  }
-
-  public <T> T thenCalledTimes(final Object numberMatcher, T mock) {
-    return overrider.override(mock, new Handler() {
-      public Object handle(Invocation invocation) {
-        thenCalledTimes(numberMatcher, wildcardSupport.matcherize(invocation));
-        return defaultValue(invocation.method.getReturnType());
-      }
-    });
-  }
-
-  public void thenCalledTimes(Object numberMatcher, InvocationMatcher invocationMatcher) {
-    int numberOfCalls = 0;
-    for (Invocation invocation : invocationHistory.get()) {
-      if (invocationMatcher.matches(invocation)) {
-        numberOfCalls++;
-      }
-    }
-    boolean expected = asMatcher(numberMatcher).matches(numberOfCalls);
-    if (!expected) {
-      throw assertionError(pageFormatter
-          .add(header("expected called times " + numberMatcher))
-          .add(body(invocationMatcher))
-          .add(header("but called"))
-          .add(body("times " + numberOfCalls))
-          .add(header("actual invocations"))
-          .add(invocationHistory.get().size() > 0
-              ? multiline(invocationHistory.get().reverse())
-              : body("none"))
-          .build());
-    }
-  }
-
-  public <T> T thenCalledInOrder(T mock) {
-    return overrider.override(mock, new Handler() {
-      public Object handle(Invocation invocation) {
-        thenCalledInOrder(wildcardSupport.matcherize(invocation));
-        return defaultValue(invocation.method.getReturnType());
-      }
-    });
-  }
-
-  public void thenCalledInOrder(InvocationMatcher invocationMatcher) {
-    Optional<VerifyingInOrder> verified = verifyInOrder(invocationMatcher, history.get());
-    if (verified.isPresent()) {
-      history.add(verified.get());
+  private static Matcher numberMatcher(Invocation invocation) {
+    if (invocation.method.getName().equals("thenCalledNever")) {
+      return numberMatcher(0);
+    } else if (invocation.method.getName().equals("thenCalled")) {
+      return numberMatcher(1);
+    } else if (invocation.method.getName().equals("thenCalledTimes")
+        && invocation.method.getParameterTypes()[0] == int.class) {
+      return numberMatcher((int) invocation.arguments.get(0));
     } else {
-      throw assertionError(pageFormatter
-          .add(header("expected called in order"))
-          .add(body(invocationMatcher))
-          .add(header("but not called"))
-          .add(header("actual invocations"))
-          .add(invocationHistory.get().size() > 0
-              ? multiline(invocationHistory.get().reverse())
-              : body("none"))
-          .build());
+      return asMatcher(invocation.arguments.get(0));
     }
   }
 
-  private static Matcher exactly(final int number) {
+  private static Matcher numberMatcher(final int number) {
     return new Matcher() {
       public boolean matches(Object item) {
         return item.equals(number);
